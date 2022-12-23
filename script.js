7c4342df91f3e683985f886a082cedf943c39b43
luca-gchs <lucagaches2@gmail.com>
2022-12-23 22:20:07 +0100

    fix dark mode

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const chk = document.querySelector('.checkbox');
 const descriptions = document.querySelectorAll('.descr');
+const descr = document.querySelectorAll('.title');
 
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
 	descriptions.forEach(item => {
+		item.classList.toggle('light')
+	})
+	descr.forEach(item =>{
 		item.classList.toggle('light')
 	})
 });

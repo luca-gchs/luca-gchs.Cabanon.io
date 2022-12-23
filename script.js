413df4097979f25c2154f32f7da749a87d503904
Valeria Proshachenko <vproshachenko@gmail.com>
2022-12-23 23:10:52 +0200

    [fix]: fix text dark mode

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const chk = document.querySelector('.checkbox');
+const descriptions = document.querySelectorAll('.descr');
 
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
+	descriptions.forEach(item => {
+		item.classList.toggle('light')
+	})
 });

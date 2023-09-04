969dd2b7daf8a0f40c7908ca3af655a8f9d7bfca
luca-gchs <lucagaches2@gmail.com>
2023-09-04 22:40:01 +0200

    creation de la login page avec singup and login nouveau fichier html ajouts style et event avec js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// dark mode
+
+
 const chk = document.querySelector('.checkbox');
 const descriptions = document.querySelectorAll('.descr');
 const descr = document.querySelectorAll('.title');
@@ -11,3 +14,42 @@ chk.addEventListener('change', () => {
 		item.classList.toggle('light')
 	})
 });
+
+
+
+// switch entre login et signup
+
+
+
+
+const formOpenBtn = document.querySelector("#form-open"),
+maison = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login"),
+pwShowHide = document.querySelectorAll(".pw_hide"); // constante nommées puis dirigées dans le code avec query
+
+
+// montrer le mdp ou pas en cliquant 
+
+pwShowHide.forEach((icon) => {
+	icon.addEventListener("click", () => {
+		let getPwInput = icon.parentElement.querySelector("input");
+		if(getPwInput.type === "password"){
+			getPwInput.type = "text";
+			icon.classList.replace("uil-eye-slash" , "uil-eye");
+		}else{
+			getPwInput.type = "password";
+			icon.classList.replace("uil-eye" , "uil-eye-slash");
+		}
+	});
+});
+
+signupBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	formContainer.classList.remove("active");
+}); // action perfomées avec click sur les constantes 
